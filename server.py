fc8ce07e474918be35e14f12628429acf9db5f97
Kwstantinos Balla <106969249+kwstantinosE19105@users.noreply.github.com>
2025-08-27 22:51:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,5 @@
-
+import socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socker.bind(("127.0.0.1",65432))
+server_socket.listen()
+print("Server is listening on 127.0.0.1:65432...")
