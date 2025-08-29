13a15e8322f759fc617c7ef23d33e68863df8bec
Kwstantinos Balla <106969249+kwstantinosE19105@users.noreply.github.com>
2025-08-29 21:44:24 +0300

    Update server.py

--- server.py
+++ server.py
@@ -35,4 +35,65 @@ def main():
                 with conn:
                     #1)filename (len + bytes)
                     filename_bytes = recv_prefixed_bytes(conn)
-                    filename = filename_bytes.decode("utf-8"
+                    filename = filename_bytes.decode("utf-8",errors="strict")
+
+                    #2)filesize (8-byte unsigned, big-endian)
+                    (filesize,) = struct.unpack(!Q",recv_exact(conn,8))
+
+
+                    #3)sha256 (32 bytes)
+                    expected_sha256 = recv_exact(conn,32)
+
+                    #4)file content (filesize bytes)
+                    dest_path = SAVE_DIR / Path(filename).name # sanitize name
+                    hasher = hashlib.sha256()
+                    received = 0
+
+                    with open(dest_path, "wb") as f:
+                        while received < filesize:
+                            chunk = conn.recv(min(1024 * 64, filesize - received))
+                            if not chunk:
+                                raise  ConnectionError("Connection dropped mid-file.)
+                            f.write(chunk)
+                            hasher.update(chunk)
+                            received += len(chunk)
+                                
+                    actual_sha256 = hasher.digest()
+                    if actual_sha256 == expected_sha256:
+                        conn.sendall(b"OK")
+                        print(f"saved {dest_path}({filesize} bytes) [OK]")
+                    else:
+                        conn.sendall(b"BAD_CHECKSUM")
+                        print(f"Checksum mismatch for {dest_path}")
+                
+
+            except Exception as e:
+                print(f"Error: {e}")
+if__name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
