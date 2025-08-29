9c5f9318a72f9358cd69bc6ed5a0fb780e211218
Kwstantinos Balla <106969249+kwstantinosE19105@users.noreply.github.com>
2025-08-29 18:45:05 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,7 @@
-
+import socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(("127.0.0.1",65432))
+client_socket.sendall(b"Hello, Server!")
+data = client_socket.recv(1024)
+print("server says:",data.decode())
+client_socket.close()
